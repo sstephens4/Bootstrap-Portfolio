9c7221194081f245d9150abcccaa888a043382ed
Serena Stephens <stephensserena@gmail.com>
2019-05-28 17:47:09 -0700

    changes

--- assets/logic.js
+++ assets/logic.js
@@ -1,1 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, options);
+  });
 
+  
